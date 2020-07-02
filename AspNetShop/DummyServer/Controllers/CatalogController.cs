25f7c299b8b988858bfa25ccfe9d8ba9b3c98a20
alexLopatin <minelabory@gmail.com>
2020-07-02 11:42:34 +0300

    added categories and search

---
+++ AspNetShop/DummyServer/Controllers/CatalogController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetShop.Shared.ModelView;
+
+namespace AspNetShop.DummyServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly DataLoader _loader;
+        public CatalogController(DataLoader loader)
+        {
+            _loader = loader;
+        }
+        [HttpGet]
+        public IEnumerable<Category> Get()
+        {
+            var list = _loader.Get<Category>();
+            return list.ToArray();
+        }
+    }
+}
