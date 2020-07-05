1fb72c93c491a2cf7cf513f0c3643c7de525f3f9
alexLopatin <minelabory@gmail.com>
2020-07-06 02:04:08 +0300

    added cart

---
+++ AspNetShop/DummyServer/Controllers/CartController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetShop.Shared.ModelView;
+
+namespace AspNetShop.DummyServer.Controllers
+{
+    [ApiController]
+    [Route("{controller}/{action=Get}")]
+    public class CartController : Controller
+    {
+        private readonly DataLoader _loader;
+        public CartController(DataLoader loader)
+        {
+            _loader = loader;
+        }
+        [HttpPost]
+        public IEnumerable<Product> Get(Dictionary<string,int> products)
+        {
+            var list = _loader.Get<Product>().Where(x => products.ContainsKey(x.Id.ToString()));
+            return list.ToArray();
+        }
+    }
+}
