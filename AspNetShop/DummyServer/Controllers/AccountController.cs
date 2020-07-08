1c95886ca0b5798f172a016e9edb6d40851738c0
alexLopatin <minelabory@gmail.com>
2020-07-08 03:41:35 +0300

    JWT WORKS

---
+++ AspNetShop/DummyServer/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using AspNetShop.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AspNetShop.DummyServer.Controllers
+{
+    [ApiController]
+    [Route("{controller}/{action}")]
+    public class AccountController : Controller
+    {
+        public AccountController()
+        {
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public string Login(AspNetShop.Shared.ModelView.Login viewModel)
+        {
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, viewModel.UserName)
+            };
+            
+            // 3. Генерируем JWT.
+            var token = new JwtSecurityToken(
+                issuer: "https://localhost",
+                audience: "https://localhost",
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: new SigningCredentials( 
+                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes("RANDOM_KEY_MUST_NOT_BE_SHARED")),
+                    SecurityAlgorithms.HmacSha256));
+ 
+            string jwtToken = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwtToken;
+
+        }
+    }
+}
---
+++ AspNetShop/Shared/ModelView/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetShop.Shared.ModelView
+{
+    public class Login
+    {
+        public string UserName { get; set; } = "";
+        public bool RememberMe { get; set; } = false;
+    }
+}
