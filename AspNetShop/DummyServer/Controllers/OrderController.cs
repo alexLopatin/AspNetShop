24a35cc5131a6f592b4d51dffb679b56c0b9c69b
alexLopatin <minelabory@gmail.com>
2020-07-10 14:13:27 +0300

    fix

--- AspNetShop/DummyServer/Controllers/OrderController.cs
+++ AspNetShop/DummyServer/Controllers/OrderController.cs
@@ -43,7 +43,6 @@ namespace AspNetShop.DummyServer.Controllers
             orders.Add(order);
             return orders.ToArray();
         }
-
         [HttpPost]
         [Authorize]
         public List<string> CreateOrder(Shared.Form.Order orderForm)
--- AspNetShop/Shared/ModelView/Stock.cs
+++ AspNetShop/Shared/ModelView/Stock.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace AspNetShop.Shared.ModelView
 {
     public class Stock
     {
-        public int Id { get; set; }
+        [Key]
+        public int StockId { get; set; }
         public string Description { get; set; }
         public string Banner { get; set; }
     }
