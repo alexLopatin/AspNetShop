8789c94234d714930f1798473459857a5aa85cef
alexLopatin <minelabory@gmail.com>
2020-07-11 17:23:07 +0300

    add register page

---
+++ AspNetShop/Shared/Form/Register.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetShop.Shared.Form
+{
+    public class Register
+    {
+        public string UserName { get; set; } = "";
+        public string FirstName { get; set; } = "";
+        public string SecondName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public bool RememberMe { get; set; } = false;
+        public string Password { get; set; }
+    }
+}
